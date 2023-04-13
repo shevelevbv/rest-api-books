71a5d557bf14db1139458951bb7147462be80ad3
sptm <me@sptm.dev>
2023-04-13 12:01:34 +0400

    fix book creation

--- src/repositories/BookRepository.ts
+++ src/repositories/BookRepository.ts
@@ -1,9 +1,10 @@
 import Book from '../entities/Book';
 import AppDataSource from '../connections/MongoConnection';
 import { IBook } from '../interfaces/IBook';
+import { InsertResult } from "typeorm";
 
 export default class BookRepository {
   static getBooks = async (): Promise<Array<IBook>> => AppDataSource.getMongoRepository(Book).find();
 
-  static addBook = async (book: Book): Promise<Book> => AppDataSource.manager.save(book);
+  static addBook = async(book: IBook): Promise<InsertResult> => AppDataSource.getRepository(Book).insert(book);
 }
--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,8 +1,8 @@
 import BookRepository from '../repositories/BookRepository';
 import { IBook } from '../interfaces/IBook';
-import Book from '../entities/Book';
+import { InsertResult } from "typeorm";
 
 export default class BookService {
   static getBooks = async (): Promise<Array<IBook>> => BookRepository.getBooks();
-  static addBook = async (book: Book): Promise<Book> => BookRepository.addBook(book);
+  static  addBook = async (book: IBook): Promise<InsertResult> => BookRepository.addBook(book);
 }
